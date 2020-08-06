2a5f61af49f59c6c7e7f44383ec87126c0c519b0
LetsCodeTogether <54933087+LetsCodeTogether07@users.noreply.github.com>
2020-08-06 18:03:39 +0530

    Here is the full code

---
+++ Project_3/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+
+using namespace std;
+
+class ATM
+{
+   int balance;
+   public:
+       ATM()
+       {
+           balance = 100;
+       }
+
+       void withdrawCash()
+       {
+           int amount;
+           cout<< "Enter amount: ";
+           cin>>amount;
+
+           if(amount > balance)
+           {
+               cout<< "Insufficient balance your balance is "<<balance;
+           }
+           else
+           {
+               cout<<"Successfully withdrawed "<<balance;
+               balance -= amount; // b = b - amount
+           }
+
+       }
+
+       void dipositCash()
+       {
+           int amount;
+           cout<< "Enter amount: ";
+           cin>>amount;
+           cout<<"Previous balance: "<<balance;
+           balance+= amount;
+           cout<<"\nCurrent balance: "<<balance;
+       }
+
+       void displayBalance()
+       {
+           cout<<"Balance: "<<balance;
+       }
+
+};
+
+int main()
+{
+    ATM user1;
+
+    int n;
+
+abc:    cout<<"\n\n1. Withdraw\n2.Diposit\n3.Display\n4. quit";
+
+    cout<<"\nEnter your choice: ";
+    cin>>n;
+
+    switch(n)
+    {
+    case 1:
+        user1.withdrawCash();
+        goto   abc;
+        break;
+
+    case 2:
+        user1.dipositCash();
+        goto   abc;
+        break;
+
+    case 3:
+        user1.displayBalance();
+        goto   abc;
+        break;
+    case 4:
+        return 0;
+
+    default:
+        cout<<"INVALID OPTION";
+        break;
+
+
+    }
+
+    return 0;
+}
